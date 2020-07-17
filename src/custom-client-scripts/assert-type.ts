76fd75114374fa25ec3459b7e4392e08cdd55d61
Aleksandar <a.nozharov@reply.de>
2020-07-17 14:46:15 +0200

    Cleanup

--- src/custom-client-scripts/assert-type.ts
+++
@@ -1,6 +0,0 @@
-import { assertType, is } from '../errors/runtime/type-assertions';
-import ClientScript from './client-script';
-
-export default function (scripts: ClientScript[]): void {
-    scripts.forEach((script: ClientScript) => assertType([is.string, is.clientScriptInitializer], 'clientScripts', `Client script`, script));
-}
